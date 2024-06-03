7573b1c4938116caafdf5d7ede089d4be7b7a86a
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-06-03 12:39:18 +0600

    added spinner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
+import Loader from "../../UI-Animation/LoadingIcon-1/Loader";
 import Loader2 from "../../UI-Animation/LoadingIcon-2/Loader2";
 
 const Home = () => {
   return (
     <div className="w-full h-full">
+      <Loader />
       <Loader2 />
     </div>
   );
