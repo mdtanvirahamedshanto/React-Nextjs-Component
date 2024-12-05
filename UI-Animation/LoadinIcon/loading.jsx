e2e344eaac8aa7a32de8c996820c0c89859ecd44
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2024-12-05 11:15:44 +0600

    add new loading

---
+++ UI-Animation/LoadinIcon/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen bg-gray-900'>
+   <div className="w-10 h-10 animate-[spin_1s_linear_infinite] rounded-full border-4 border-r-[#3B9DF8] border-[#3b9df84b]"></div>
+    </div>
+  )
+}
+
+export default loading
